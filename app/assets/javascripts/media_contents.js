626b3037489ee8650af8ce151d2c3043fe82aa0d
RememberLenny <leonard_bogdonoff@condenast.com>
2014-11-16 11:28:08 -0500

    Get carrierwave to upload a file

---
+++ app/assets/javascripts/media_contents.js
@@ -0,0 +1,9 @@
+
+$(function() {
+  var mediaDropzone;
+  mediaDropzone = new Dropzone("#media-dropzone");
+  return mediaDropzone.on("success", function(file, responseText) {
+    var imageUrl;
+    imageUrl = responseText.file_name.url;
+  });
+});
